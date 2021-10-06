bf01dba48c3bfba6dd36801b897ac995babb944a
paragkapoorcs <83000158+paragkapoorcs@users.noreply.github.com>
2021-10-06 23:08:11 +0530

    Update bqdataset.tf

--- automation/04_BigQuery_Dataset/bqdataset.tf
+++ automation/04_BigQuery_Dataset/bqdataset.tf
@@ -27,3 +27,63 @@ resource "google_bigquery_dataset" "bq-data-set" {
          special_group = "projectWriters"
         }
 }
+
+resource "google_bigquery_dataset" "bq-data-set" {
+     dataset_id                      = var.dimention_dataset
+     default_partition_expiration_ms = 0
+     delete_contents_on_destroy      = false
+     description                     = "SCM Dataset"
+     friendly_name                   = var.dimention_name
+     labels                          = {
+         "env" = "default"
+        }
+     location                        = "EU"
+     project                         = var.project_id
+
+     access {
+         role          = "OWNER"
+         user_by_email = var.owner_email
+        }
+     access {
+         role          = "OWNER"
+         special_group = "projectOwners"
+        }
+     access {
+         role          = "READER"
+         special_group = "projectReaders"
+        }
+     access {
+         role          = "WRITER"
+         special_group = "projectWriters"
+        }
+}
+
+resource "google_bigquery_dataset" "bq-data-set" {
+     dataset_id                      = var.fact_dataset
+     default_partition_expiration_ms = 0
+     delete_contents_on_destroy      = false
+     description                     = "SCM Dataset"
+     friendly_name                   = var.fact_name
+     labels                          = {
+         "env" = "default"
+        }
+     location                        = "EU"
+     project                         = var.project_id
+
+     access {
+         role          = "OWNER"
+         user_by_email = var.owner_email
+        }
+     access {
+         role          = "OWNER"
+         special_group = "projectOwners"
+        }
+     access {
+         role          = "READER"
+         special_group = "projectReaders"
+        }
+     access {
+         role          = "WRITER"
+         special_group = "projectWriters"
+        }
+}
