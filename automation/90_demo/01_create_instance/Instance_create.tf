a89ca44ad003f4dbd2518e9b6699cf4f5e75ac54
paragkapoorcs <83000158+paragkapoorcs@users.noreply.github.com>
2021-10-07 17:36:02 +0530

    Update Instance_create.tf

--- automation/90_demo/01_create_instance/Instance_create.tf
+++ automation/90_demo/01_create_instance/Instance_create.tf
@@ -1,3 +1,9 @@
+#Copyright 2021 Google Inc. All rights reserved.
+
+#The use of this software is governed by the Pre-GA Offering 
+#Terms section of the the Service Specific Terms set forth at 
+#https://cloud.google.com/terms/service-terms#general-service-terms
+
 terraform {
   required_version = ">= 0.14"
 
